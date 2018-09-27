3e49928cddbd4a4f0d0bee8c1d59648d63cbb6f5
Simone Gentili <sensorario@gmail.com>
2018-09-27 17:51:14 +0200

    fix autoloader

--- scripts/FightToTheDeath.php
+++ scripts/FightToTheDeath.php
@@ -1,6 +1,6 @@
 <?php
 
-include_once __DIR__ . '/vendor/autoload.php';
+include_once __DIR__ . '/../vendor/autoload.php';
 
 use Sensorario\Develog\Logger\NormaLogger;
 use Sensorario\DnD\Actions\Fight;
